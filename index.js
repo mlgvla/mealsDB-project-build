8c9e299c74423a17f1f890e197d055682d9f7aae
Monica Gerard <mlgvla@gmail.com>
2023-06-06 20:17:11 -0400

    relocates globals to index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+// Elements
+const cuisineSelect = document.querySelector("#cuisines")
+const categorySelect = document.querySelector("#categories")
+
+// Function Calls
+getCuisines()
+getCategories()
+
+// Event Listeners
+cuisineSelect.addEventListener("change", getRecipesByCuisine)
+categorySelect.addEventListener("change", getRecipesByCategory)
+
 function getRecipesByCuisine(e) {
-    const cuisine = e.target.value
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cuisine}`)
-    .then(r => r.json())
-    .then(recipes => console.log(recipes))
+   const cuisine = e.target.value
+   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cuisine}`)
+      .then(r => r.json())
+      .then(recipes => console.log(recipes))
 }
 
 function getRecipesByCategory(e) {
-    const category = e.target.value
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
-    .then(r => r.json())
-    .then(categories => console.log(categories))
+   const category = e.target.value
+   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+      .then(r => r.json())
+      .then(categories => console.log(categories))
 }
--- utilities.js
+++ utilities.js
@@ -1,16 +1,20 @@
-getCuisines()
-getCategories()
 
+
+
+// Fetch Functions
 function getCuisines() {
    fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
       .then(r => r.json())
       .then(cuisines => renderCuisineOptions(cuisines))
 }
+function getCategories() {
+   fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+      .then(r => r.json())
+      .then(categories => renderCategoryOptions(categories))
+}
 
+// Render Functions
 function renderCuisineOptions(cuisines) {
-   const cuisineSelect = document.querySelector("#cuisines")
-   cuisineSelect.addEventListener("change", getRecipesByCuisine)
-
    cuisines.meals.forEach(cuisine => {
       const option = document.createElement("option")
       option.value = cuisine.strArea
@@ -19,21 +23,12 @@ function renderCuisineOptions(cuisines) {
    })
 }
 
-function getCategories() {
-    fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-    .then(r => r.json())
-    .then(categories => renderCategoryOptions(categories))
+function renderCategoryOptions(categories) {
+   categories.meals.forEach(category => {
+      const option = document.createElement("option")
+      option.value = category.strCategory
+      option.textContent = category.strCategory
+      categorySelect.append(option)
+   })
 }
 
-function renderCategoryOptions(categories) {
-    const categorySelect = document.querySelector("#categories")
-    categorySelect.addEventListener("change", getRecipesByCategory)
-    
-    categories.meals.forEach(category => {
-       const option = document.createElement("option")
-       option.value = category.strCategory
-       option.textContent = category.strCategory
-       categorySelect.append(option)
-    })
-    
-}
