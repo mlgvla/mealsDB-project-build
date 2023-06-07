d9496f1bde22996ef891428343c166a490791e54
Monica Gerard <mlgvla@gmail.com>
2023-06-06 20:39:25 -0400

    shows recipe images and titles in recipe container

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Elements
 const cuisineSelect = document.querySelector("#cuisines")
 const categorySelect = document.querySelector("#categories")
+const recipeContainer = document.querySelector(".recipe-container")
+const selectionH1 = document.querySelector(".selection-heading")
 
 // Function Calls
 getCuisines()
@@ -14,12 +16,33 @@ function getRecipesByCuisine(e) {
    const cuisine = e.target.value
    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cuisine}`)
       .then(r => r.json())
-      .then(recipes => console.log(recipes))
+      .then(recipes => displayRecipes(recipes))
 }
 
 function getRecipesByCategory(e) {
    const category = e.target.value
    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
       .then(r => r.json())
-      .then(categories => console.log(categories))
+      .then(categories => displayRecipes(categories))
+}
+
+function displayRecipes(recipes) {
+   recipeContainer.replaceChildren()
+   console.log(recipes)
+   selectionH1.textContent = cuisineSelect.value || categorySelect.value
+
+   recipes.meals.forEach(recipe => renderRecipeCard(recipe))
+}
+
+function renderRecipeCard(recipe) {
+   const recipeImg = document.createElement("img")
+   recipeImg.src = recipe.strMealThumb
+   recipeImg.alt = recipe.strMeal
+
+   const recipeTitle = document.createElement("p")
+   recipeTitle.textContent = recipe.strMeal
+
+   const recipeId = recipe.idMeal
+
+   recipeContainer.append(recipeImg, recipeTitle)
 }
